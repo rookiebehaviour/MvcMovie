a636db391e308c00f4519307a74e412d18480d1c
Jacqueline M <75797321+rookiebehaviour@users.noreply.github.com>
2023-08-20 15:52:55 -0400

    Updated movie model.

--- MvcMovie/Models/Movie.cs
+++ MvcMovie/Models/Movie.cs
@@ -12,9 +12,9 @@ namespace MvcMovie.Models
         [Display(Name = "Release Date"), DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
         [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$"), Required, StringLength(30)]
-        public string Genre { get; set; }
+        public string? Genre { get; set; }
         [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$"), StringLength(5)]
-        public string Rating { get; set; }
+        public string? Rating { get; set; }
 
         [Range(1, 100), DataType(DataType.Currency)]
         [Column(TypeName = "decimal(18, 2)")]
